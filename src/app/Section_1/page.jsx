9f17cc001c45bd04e29c9539f32217916229c987
ZRUBIX-Gowtham <gowtham@zrubix.com>
2025-09-13 11:27:29 +0530

    V29

--- src/app/Section_1/page.jsx
+++ src/app/Section_1/page.jsx
@@ -6,6 +6,7 @@ import V15 from './V15';
 import V16 from './V16';
 import MobileV16 from './MobileV16';
 import V26 from './V26';
+import V29 from './V29';
 
 export default function Section_2() {
 
@@ -27,30 +28,37 @@ export default function Section_2() {
     <div id="section2-container">
       <h2 style={{ textAlign: 'center', fontWeight: '700' }}>{titleText}</h2>
       <section id="V12" style={{ padding: '100px 0 0 0', textAlign: 'center', fontWeight: 'bold' }}>
-        <label>Section_2 - V12</label>
+        <label>Section_1 - V12</label>
         <div>
           <V12 />
         </div>
       </section>
 
       <section id="V15" style={{ padding: '20px 0 0 0', textAlign: 'center', fontWeight: 'bold' }}>
-        <label>Section_2 - V15</label>
+        <label>Section_1 - V15</label>
         <div>
           <V15 />
         </div>
       </section>
 
       <section id="V16" style={{ padding: '20px 0 0 0', textAlign: 'center', fontWeight: 'bold' }}>
-        <label>Section_2 - V16</label>
+        <label>Section_1 - V16</label>
         <div>
           <V16/>
         </div>
       </section>
 
        <section id="V26" style={{ padding: '20px 0 0px 0', textAlign: 'center', fontWeight: 'bold' }}>
-        <label>Section_2 - V26</label>
+        <label>Section_1 - V26</label>
         <div>
           <V26/>
+        </div>
+      </section>
+
+      <section id="V29" style={{ padding: '20px 0 0px 0', textAlign: 'center', fontWeight: 'bold' }}>
+        <label>Section_1 - V29</label>
+        <div>
+          <V29/>
         </div>
       </section>
     </div>
